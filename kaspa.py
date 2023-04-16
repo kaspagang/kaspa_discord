6c90d7cbec164a5f546ab27b1d4a2a9d80304ded
D-Stacks <78099568+D-Stacks@users.noreply.github.com>
2023-04-16 14:05:07 +0200

    Update kaspa.py

--- kaspa.py
+++ kaspa.py
@@ -49,8 +49,8 @@ def get_stats(use_dedicated_node=TRY_DEDICATED_NODE, tries = 0):
   try:
     stats = dict()
     blockdag_info = cli.request('getBlockDagInfoRequest',timeout=4)['getBlockDagInfoResponse']
-    stats['block_count'] = blockdag_info['blockCount']
-    stats['header_count'] = blockdag_info['headerCount']
+    #stats['block_count'] = blockdag_info['blockCount']
+    #stats['header_count'] = blockdag_info['headerCount']
     stats['pruning_point'] = blockdag_info['pruningPointHash']
     stats['parent_hashes'] = blockdag_info['virtualParentHashes']
     stats['tip_hashes'] = blockdag_info['tipHashes']
